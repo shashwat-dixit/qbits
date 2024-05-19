92b994597c33ad1cc6262861131ca4a54bb090c2
Shashwat Dixit <shashwatmain@gmail.com>
2024-05-19 22:06:15 +0530

    Adding routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,34 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+//layouts
+import RootLayout from "./layouts/RootLayout";
+
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 
 
 export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    )
+  );
+  
+
   return (
-    <div>App</div>
+    <RouterProvider
+    router={router}
+    // fallbackElement={<BigSpinner />}
+  />
   )
 }
